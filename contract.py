2b4cc87bc8b1f0da0b9ae53ea7c0afee83df7850
cryptofunny724 <cryptofunny724@gmail.com>
2026-06-15 13:21:52 +0200

    Update contract.py v2.0 - Added Token connection

--- contract.py
+++ contract.py
@@ -5,16 +5,30 @@ from genlayer import *
 
 class ReputationScoring(gl.Contract):
     scores: TreeMap[Address, u256]
-    # token_address: Address   # فعلاً کامنت شد تا Deploy راحت بشه
+    token_address: Address
 
     def __init__(self):
-        pass
+        # Default token address (can be updated later)
+        self.token_address = Address("0x698c060E742D37E4742aEf4d790ba1543325C15b")
+
+    @gl.public.write
+    def set_token_address(self, new_token: Address) -> None:
+        """Update token address if needed"""
+        self.token_address = new_token
 
     @gl.public.write
     def record_action(self, value: u256) -> None:
         user = gl.message.sender_address
         current = self.scores.get(user, u256(0))
         self.scores[user] = current + value
+
+        # Auto reward token
+        if value > u256(0):
+            try:
+                token = gl.contract(self.token_address)
+                token.mint(value // u256(2))
+            except:
+                pass
 
     @gl.public.view
     def my_score(self) -> u256:
