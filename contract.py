a6f054edb07064726635859314fb839f8e6e36d9
cryptofunny724 <cryptofunny724@gmail.com>
2026-06-15 16:47:42 +0200

    Update contract.py to final stable version

--- contract.py
+++ contract.py
@@ -1,4 +1,4 @@
-# v2.0
+# v2.3 Stable
 # { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
 
 from genlayer import *
@@ -11,13 +11,11 @@ class ReputationScoring(gl.Contract):
 
     @gl.public.write
     def record_action(self, value: u256) -> None:
-        """Record action and update reputation score"""
         user = gl.message.sender_address
         current = self.scores.get(user, u256(0))
         self.scores[user] = current + value
 
     @gl.public.view
     def my_score(self) -> u256:
-        """Get my reputation score"""
         user = gl.message.sender_address
         return self.scores.get(user, u256(0))
