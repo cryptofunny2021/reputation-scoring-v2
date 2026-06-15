e5a6fca01eedb4d75b0f5124787693f136d7010d
cryptofunny724 <cryptofunny724@gmail.com>
2026-06-15 14:08:59 +0200

    Finalize Reputation Scoring v2.0 - Stable version

--- contract.py
+++ contract.py
@@ -5,26 +5,19 @@ from genlayer import *
 
 class ReputationScoring(gl.Contract):
     scores: TreeMap[Address, u256]
-    token_address: Address
 
-    def __init__(self, token_addr: Address):
-        self.token_address = token_addr
+    def __init__(self):
+        pass
 
     @gl.public.write
     def record_action(self, value: u256) -> None:
+        """Record action and update reputation score"""
         user = gl.message.sender_address
         current = self.scores.get(user, u256(0))
         self.scores[user] = current + value
 
-        # Auto reward token
-        if value > u256(0):
-            try:
-                token = gl.contract(self.token_address)
-                token.mint(value // u256(2))
-            except:
-                pass
-
     @gl.public.view
     def my_score(self) -> u256:
+        """Get my reputation score"""
         user = gl.message.sender_address
         return self.scores.get(user, u256(0))
